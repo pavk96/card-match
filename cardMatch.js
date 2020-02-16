2ca579970ca9655dc0f4cae485d8a058ac2e8f58
pavk96 <56814290+pavk96@users.noreply.github.com>
2020-02-16 11:54:09 +0900

    Update cardMatch.js

--- cardMatch.js
+++ cardMatch.js
@@ -1,55 +1,59 @@
 var 가로=4;
 var 세로=3;
-var 이미지후보1=[
-'C:/Users/user/Desktop/web/a.jpg',
-'C:/Users/user/Desktop/web/b.png',
-'C:/Users/user/Desktop/web/a.jpg',
-'C:/Users/user/Desktop/web/c.jpg',
-'C:/Users/user/Desktop/web/d.jpg',
-'C:/Users/user/Desktop/web/c.jpg',
-'C:/Users/user/Desktop/web/d.jpg',
-'C:/Users/user/Desktop/web/e.jpg',
-'C:/Users/user/Desktop/web/b.png',
-'C:/Users/user/Desktop/web/f.jpg',
-'C:/Users/user/Desktop/web/e.jpg',
-'C:/Users/user/Desktop/web/f.jpg'];
-var 이미지후보2=['C:/Users/user/Desktop/web/1.jpg',
-'C:/Users/user/Desktop/web/2.jpg',
-'C:/Users/user/Desktop/web/3.jpg',
-'C:/Users/user/Desktop/web/4.jpg',
-'C:/Users/user/Desktop/web/5.jpg',
-'C:/Users/user/Desktop/web/6.jpg',
-'C:/Users/user/Desktop/web/7.jpg',
-'C:/Users/user/Desktop/web/8.jpg',
-'C:/Users/user/Desktop/web/9.jpg',
-'C:/Users/user/Desktop/web/10.jpg',
-'C:/Users/user/Desktop/web/11.jpg',
-'C:/Users/user/Desktop/web/12.jpg']
-var 이미지=[];
+// var 이미지후보1=[
+// 'C:/Users/user/Desktop/web/a.jpg',
+// 'C:/Users/user/Desktop/web/b.png',
+// 'C:/Users/user/Desktop/web/a.jpg',
+// 'C:/Users/user/Desktop/web/c.jpg',
+// 'C:/Users/user/Desktop/web/d.jpg',
+// 'C:/Users/user/Desktop/web/c.jpg',
+// 'C:/Users/user/Desktop/web/d.jpg',
+// 'C:/Users/user/Desktop/web/e.jpg',
+// 'C:/Users/user/Desktop/web/b.png',
+// 'C:/Users/user/Desktop/web/f.jpg',
+// 'C:/Users/user/Desktop/web/e.jpg',
+// 'C:/Users/user/Desktop/web/f.jpg'];
+// var 이미지후보2=['C:/Users/user/Desktop/web/1.jpg',
+// 'C:/Users/user/Desktop/web/2.jpg',
+// 'C:/Users/user/Desktop/web/3.jpg',
+// 'C:/Users/user/Desktop/web/4.jpg',
+// 'C:/Users/user/Desktop/web/5.jpg',
+// 'C:/Users/user/Desktop/web/6.jpg',
+// 'C:/Users/user/Desktop/web/7.jpg',
+// 'C:/Users/user/Desktop/web/8.jpg',
+// 'C:/Users/user/Desktop/web/9.jpg',
+// 'C:/Users/user/Desktop/web/10.jpg',
+// 'C:/Users/user/Desktop/web/11.jpg',
+// 'C:/Users/user/Desktop/web/12.jpg'];
+// var 이미지=[];
+var 색깔후보 =['red','red','blue','blue','pink','pink','yellow','yellow','green','green','black','black'];
+var 색깔 = [];
 var 클릭플래그 = true;
 var 클릭카드 =[];
 var 완성카드 = [];
-  //for(var i=0;i<이미지후보1.length;i+=1){
-//    이미지 = 이미지.concat(이미지후보1.splice(Math.floor(Math.random()*이미지후보1.length), 1));
-//  }
-//console.log(이미지);
+for(var i=0;i<색깔.length;i+=1){
+    색깔 = 색깔.concat(색깔후보.splice(Math.floor(Math.random()*색깔후보.length), 1));
+  }
+console.log(색깔);
 
 
-function 카드세팅(이미지후보1){
+function 카드세팅(가로,세로){
   클릭플래그 = false;
-  for (var i = 0; i<이미지후보1.length;i+=1){
+  for (var i = 0; i<가로*세로;i+=1){
     var card=document.createElement('div');
     card.className='card';
     var cardInner=document.createElement('div');
     cardInner.className ='card-inner';
     var cardFront=document.createElement('div');
     cardFront.className='card-front';
-    cardFront.style.backgroundImage = "url('" + 이미지후보2[i] + "')";
-    cardFront.style.backgroundSize = 'cover';
+    //cardFront.style.backgroundImage = "url('" + 이미지후보2[i] + "')";
+    //cardFront.style.backgroundSize = 'cover';
+    
     var cardBack=document.createElement('div');
     cardBack.className='card-back';
-    cardBack.style.backgroundImage = "url('" + 이미지후보1[i] + "')";
-    cardBack.style.backgroundSize = 'cover';
+    //cardBack.style.backgroundImage = "url('" + 이미지후보1[i] + "')";
+    cardBack.style.backgroundColor =색깔[i];
+    //cardBack.style.backgroundSize = 'cover';
     cardInner.appendChild(cardFront);
     cardInner.appendChild(cardBack);
     card.appendChild(cardInner);
@@ -59,7 +63,7 @@ function 카드세팅(이미지후보1){
           c.classList.toggle('flipped');
           클릭카드.push(c);
           if(클릭카드.length===2){
-            if(클릭카드[0].querySelector('.card-back').style.backgroundImage===클릭카드[1].querySelector('.card-back').style.backgroundImage) {
+            if(클릭카드[0].querySelector('.card-back').style.backgroundColor===클릭카드[1].querySelector('.card-back').style.backgroundColor) {
               완성카드.push(클릭카드[0]);
               완성카드.push(클릭카드[1]);
               클릭카드=[];
@@ -97,4 +101,4 @@ function 카드세팅(이미지후보1){
 
 
 
-카드세팅(이미지후보1);
+카드세팅(가로,세로);
